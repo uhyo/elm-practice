cc31d9f5e12e006045656d2f794cde8fa4f979f3
uhyo <uhyo@uhy.ooo>
2019-07-15 22:28:45 +0900

    page2

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,9 @@
 module Main exposing (Model, Msg(..), init, main, update, view)
 
 import Browser
-import Html exposing (Html, button, div, span, text)
-import Html.Events exposing (onClick)
+import Html exposing (Html, article, button, div, h1, input, span, text)
+import Html.Attributes exposing (value)
+import Html.Events exposing (onClick, onInput)
 
 
 main =
@@ -15,6 +16,7 @@ main =
 
 type Page
     = Page1 Int
+    | Page2 String
 
 
 type alias Model =
@@ -30,10 +32,23 @@ page1update msg counter =
         Page1Decrement ->
             counter - 1
 
+        _ ->
+            counter
+
+
+page2update : Msg -> String -> String
+page2update msg text =
+    case msg of
+        Page2TextChange str ->
+            str
+
+        _ ->
+            text
+
 
 init : Model
 init =
-    Page1 0
+    Page2 "hi"
 
 
 
@@ -43,6 +58,7 @@ init =
 type Msg
     = Page1Increment
     | Page1Decrement
+    | Page2TextChange String
 
 
 update : Msg -> Model -> Model
@@ -51,18 +67,37 @@ update msg model =
         Page1 v ->
             Page1 (page1update msg v)
 
+        Page2 v ->
+            Page2 (page2update msg v)
+
 
 
 -- VIEW
 
 
+pagewrapper : String -> List (Html m) -> Html m
+pagewrapper title p =
+    article []
+        (h1
+            []
+            [ text title ]
+            :: p
+        )
+
+
 page1view : Int -> Html Msg
 page1view counter =
-    div []
+    pagewrapper "Counter Button"
         [ button [ onClick Page1Decrement ] [ text "-" ]
         , span [] [ text (" " ++ String.fromInt counter ++ " ") ]
         , button [ onClick Page1Increment ] [ text "+" ]
         ]
+
+
+page2view : String -> Html Msg
+page2view text =
+    pagewrapper "Text Field"
+        [ input [ value text, onInput Page2TextChange ] [] ]
 
 
 view : Model -> Html Msg
@@ -70,3 +105,6 @@ view model =
     case model of
         Page1 value ->
             page1view value
+
+        Page2 value ->
+            page2view value
